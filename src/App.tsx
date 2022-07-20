f326d1e35d5207922a8ba1175cde27290a03372a
Brandon McCraw <brandon.mccraw@provisionsgroup.com>
2022-07-19 22:25:43 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "@cloudscape-design/global-styles/index.css" 
+import Button from "@cloudscape-design/components/button"
+import ButtonDropdown from '@cloudscape-design/components/button-dropdown';
+import Flashbar from '@cloudscape-design/components/flashbar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonDropdown 
+          items={[
+            {text: "Go Left <", id: "goLeft", disabled: false},
+            {text: "Go Right >", id: "goLeft", disabled: false},
+            {text: "Go Up ^", id: "goLeft", disabled: false},
+          ]}
+          >Choose Your Direction</ButtonDropdown>
+          <Button onClick={save}>Save</Button>
       </header>
     </div>
   );
 }
+
+function save() {
+  return Flashbar(
+    {
+    items: [{
+      id: "message_1",
+      content: (<>"this is content"</>),
+      type: "info",
+      dismissible: true,
+      dismissLabel: "Dismiss Message"
+    }]})
+};
 
 export default App;
